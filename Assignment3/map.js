2cc6e2d0ef0a4bcdc79f2458c2fcf5b9dfb66682
Vedsted <jonso16@student.sdu.dk>
2019-10-02 19:49:11 +0200

    minor refactor

--- Assignment3/map.js
+++ Assignment3/map.js
@@ -4,18 +4,6 @@ var routeGPS = [];
 var routeMobile = [];
 
 
-var lineReader = require('readline').createInterface({
-    input: require('fs').createReadStream('Driving.csv')
-  });
-  
-	lineReader.on('line', function (line) {
-	line = line.split(',');
-	line = line.map(l => parseFloat(l));
-  	routeGPS.push([line[1],line[2]]);
-  	routeMobile.push([line[3],line[4]]);
-});
-
-
 function createMap(){
 
 	mymap = L.map('mapid').setView([55.368877, 10.428296], 13);
@@ -28,35 +16,42 @@ function createMap(){
 	}).addTo(mymap);
 }
 
+function drawRoute() {  
+	
+
+	var lineReader = require('readline').createInterface({
+		input: require('fs').createReadStream('Walking.csv')
+	});
+
+	lineReader.on('line', function (line) {
+		line = line.split(',');
+		line = line.map(l => parseFloat(l));
+		  routeGPS.push([line[1],line[2]]);
+		  routeMobile.push([line[3],line[4]]);
+	});	
 
 
+	lineReader.on('close', () => {
 
-lineReader.on('close', () => {
-	
-	//routeGPS.shift();
-	routeMobile.shift();
-	
-	//routeMobile = meanFilter(routeMobile, 10);
-	routeMobile = medianFilter(routeMobile, 10);
-	
-	//var polylineGPS = L.polyline(routeGPS, {color: 'red'}).addTo(mymap);
-	var polylineMobile = L.polyline(routeMobile, {color: 'Blue'}).addTo(mymap);
-	
-	// zoom the map to the polyline
-	mymap.fitBounds(polylineMobile.getBounds());
-});
+		//routeGPS.shift();
+		routeMobile.shift();
 
+		//routeMobile = meanFilter(routeMobile, 5);
+		routeMobile = medianFilter(routeMobile, 5);
+
+		//var polylineGPS = L.polyline(routeGPS, {color: 'red'}).addTo(mymap);
+		var polylineMobile = L.polyline(routeMobile, {color: 'Blue'}).addTo(mymap);
+
+		// zoom the map to the polyline
+		mymap.fitBounds(polylineMobile.getBounds());
+	});
 }
 
 function meanFilter(positions, n){
 	var ret = [];
 	for (var i=0; i < positions.length; i+= n) {
-		arr = positions.slice(i,i+n);
-
-		ret.push(arr.reduce((l1,l2) => {
-			return [l1[0] + l2[0],l1[1] + l2[1]];
-		}).map(e => e/arr.length));
-
+		arr = positions.slice(i,i+n).filter(e => e != null);
+		ret.push(arr.reduce((l1,l2) => [l1[0] + l2[0],l1[1] + l2[1]]).map(e => e/arr.length));
 	}
 	return ret;
 }
@@ -65,15 +60,13 @@ function meanFilter(positions, n){
 function medianFilter(positions, n){
 	var ret = [];
 	for (var i=0; i < positions.length; i+= n) {
-		arr = positions.slice(i,i+n);
+		arr = positions.slice(i,i+n).filter(e => e != null);
 
-		var lats = arr.map(e => e[0]).sort().filter(e => e != null);
-		var longs = arr.map(e => e[1]).sort().filter(e => e != null);
+		var lats = arr.map(e => e[0]).sort();
+		var longs = arr.map(e => e[1]).sort();
 
 		var ll = Math.floor(lats.length/2)
-		var latLong = [lats[ll], longs[ll]];
-		
-		ret.push(latLong);
+		ret.push([lats[ll], longs[ll]]);
 	}
 	
 	return ret;
